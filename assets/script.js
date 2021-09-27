5971475f3f19eb4860a870f780c77a3b758f8d78
Zac Goad <zjgoad@gmail.com>
2021-09-27 00:15:56 -0400

    fetch requests

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,62 @@
 // JS for WeatherTrackerHW5
 
-var APIKey = "3366ba824d39145eca7065c7319052ea";
+var APIKey = "0e433a518a861211a71db90fce0c7d3d";
+
+var chosenCity = JSON.parse(localStorage.getItem('searchedCity'));
+// localStorage.setItem('searchedCity', JSON.stringify(input));
+$('#searchButton').on('click', function(){
+    var input = $('#searchBar').val();
+    city = input;
+    weatherData(city);
+})
+
+// Displaying the cities for the user
+
+var cityArray = [];
+
+function showCities(){
+    $('#cityList').empty();
+    if (cityArray != null){
+        while (cityArray.length > 10){
+            chosenCity = JSON.parse(localStorage.getItem('cityListItem'))
+            cityArray.pop();
+            localStorage.setItem('cityListItem', JSON.stringify(chosenCity))
+        }
+    } if (cityArray == null){
+        
+    }
+}
+
+// Weather Variables
 
 
+var maxTemp;
+var minTemp;
+var windSpeed;
+var humidity;
+var uvi;
+var lat;
+var lon;
+
+
+function weatherData(city){
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid-'+APIKey+'&units=imperial'
+    fetch(weatherUrl , {
+        method: 'GET',
+        credentials: 'omit',
+        redirect: 'error'
+    })
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+}
+
+function forecastWeatherData(){
+    var fullWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude-hourly,minutely&appid='+APIKey+'&units=imperial'
+    fetch(fullWeatherUrl , {
+        method: 'GET',
+        credentials: 'omit',
+        redirect: 'error'
+    })
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+}
